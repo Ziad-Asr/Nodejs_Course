9ecf576ca3e1a37b71ab7ca73722762269e90bce
Ziad <ZiadElsayedAsr@gmail.com>
2024-01-19 04:09:30 +0200

    update

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const { dirname } = require("path");
 const url = require("url");
 
 ////////////////////////////////////////
@@ -32,12 +33,21 @@ const url = require("url");
 
 ////////////////////////////////////////
 // Server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === "/" || pathName === "/overview") {
     res.end("This is the OVERVIEW.");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT.");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",

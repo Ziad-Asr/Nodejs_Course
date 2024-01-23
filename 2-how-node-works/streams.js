6e80c385ee15d99900e75e5d86dd7cd7b49cdc28
Ziad <ZiadElsayedAsr@gmail.com>
2024-01-23 10:04:12 +0200

    update

--- 2-how-node-works/streams.js
+++ 2-how-node-works/streams.js
@@ -9,6 +9,9 @@ server.on("request", (req, res) => {
   //   });
   //   // That solution consumes the memory
   //  //(by reading the whole file and store it in a var and then starts to print it)
+
+  //
+
   // Solution2: Sreams
   //   const readable = fs.createReadStream("./test-file.txt");
   //   readable.on("data", (chunk) => {
@@ -18,13 +21,15 @@ server.on("request", (req, res) => {
   //     res.end();
   //   });
   //   readable.on("error", (err) => {
-  //     res.statusCode = 500;
+  //     res.statusCode = 500; // Server error
   //     console.log(err);
   //     res.end("File not found");
   //   });
   // // This solution is good and it does not load all data into memory as solution1, but works with steams
   // // But it also have a problem, that it is a little slower, due to back preture made by these alot of streams
   // // (Solution is to use {{pip stream}})
+
+  //
 
   // Solution3: Streaming with pip stream
   const readable = fs.createReadStream("./test-file.txt");

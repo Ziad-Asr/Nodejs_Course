7e606ef0d83e5d74da5f4b5dca5bdde769499962
Ziad <ZiadElsayedAsr@gmail.com>
2024-01-28 04:00:17 +0200

    update

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -1,14 +1,21 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) return reject('Could not find this file');
+      resolve(data);
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-
+    .then((res) => {
       console.log(res.body.message);
 
       fs.writeFile('dog-img.txt', res.body.message, (err) => {
@@ -16,5 +23,27 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
         console.log('Random dog image saved to file.');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
+
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   console.log(`Breed: ${data}`);
+
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .then((res) => {
+//       console.log(res.body.message);
+
+//       fs.writeFile('dog-img.txt', res.body.message, (err) => {
+//         if (err) return console.log(err.message);
+
+//         console.log('Random dog image saved to file.');
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// });
